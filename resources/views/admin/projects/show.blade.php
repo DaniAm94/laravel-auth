2bd8c24548be5507b1f421320ca8fbfe58a17af3
DaniAm94 <dani.amico94@gmail.com>
2024-03-19 18:09:45 +0100

    updated show project view

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('title', 'Projects')
+
+@section('content')
+    <header>
+        <h1 class="text-center my-3 ">{{ $project->title }}</h1>
+    </header>
+    <hr>
+    <div class="clearfix">
+        @if ($project->image)
+            <img src="{{ $project->image }}" alt="{{ $project->title }}" class="me-3 float-start">
+        @endif
+        <p>{{ $project->description }}</p>
+        <div>
+            <strong>Data creazione: </strong> {{ $project->created_at }}
+            <strong class="ms-3">Ultima modifica: </strong> {{ $project->updated_at }}
+        </div>
+    </div>
+    <hr>
+    <footer class="d-flex justify-content-between align--items-center">
+        <a href="{{ route('admin.projects.index') }}" class="btn btn-sm btn-secondary">
+            <i class="fa-solid fa-rotate-left"></i>
+            Torna indietro
+        </a>
+        <div class="d-flex justify-content-between gap-3">
+            <a href="{{ route('admin.projects.edit', $project->id) }}" class="btn btn-sm btn-warning">
+                <i class="fas fa-pencil"></i>
+                Modifica
+            </a>
+            <form action="{{ route('admin.projects.destroy', $project->id) }}" method="post" class="delete-form">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-sm btn-danger">
+                    <i class="far fa-trash-can"></i>
+                    Elimina</button>
+            </form>
+        </div>
+
+    </footer>
+    {{-- Delete Modal --}}
+    @include('includes.modal_confirmation_delete')
+@endsection
+
+@section('scripts')
+    @vite('resources/js/delete_confirmation.js')
+@endsection
