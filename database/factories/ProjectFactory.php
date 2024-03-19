ea7f95fda47882f0dc1cb6dd50b0dcbc3554756f
DaniAm94 <dani.amico94@gmail.com>
2024-03-19 16:29:39 +0100

    created a project resource with model, factory, controller and migration

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->text(20);
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'description' => fake()->paragraphs(15, true),
+            'image' => fake()->imageUrl(250, 250, true),
+        ];
+    }
+}
