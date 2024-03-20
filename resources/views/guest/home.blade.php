97a324904a19b0db6e7357dc1c50b693bad77a9d
DaniAm94 <dani.amico94@gmail.com>
2024-03-20 23:01:58 +0100

    added a link to move to the detail page into the guests' home

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -13,8 +13,9 @@
     <hr>
     @forelse ($projects as $project)
         <div class="card my-5">
-            <div class="card-header">
+            <div class="card-header d-flex justify-content-between align-items-center ">
                 {{ $project->title }}
+                <a href="{{ route('guest.projects.show', $project->slug) }}" class="btn btn-sm btn-primary ">Vedi</a>
             </div>
             <div class="card-body">
                 <div class="row">
@@ -25,8 +26,9 @@
                     @endif
                     <div class="col">
                         <h5 class="card-title mb-4 ">{{ $project->title }}</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ $project->created_at }}</h6>
-                        <p class="card-text">{{ $project->description }}</p>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ $project->getFormattedDate('created_at') }}
+                        </h6>
+                        <p class="card-text">{{ $project->getAbstract(100) }}</p>
                     </div>
                 </div>
             </div>
