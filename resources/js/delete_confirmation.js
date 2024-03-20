64c01a271eed904f16b389c61c78690fd9cdf878
DaniAm94 <dani.amico94@gmail.com>
2024-03-20 18:47:54 +0100

    added docs to the delete confirmation js sheet

--- resources/js/delete_confirmation.js
+++ resources/js/delete_confirmation.js
@@ -1,12 +1,18 @@
+// Lista pulsanti di cancellazione
 const deleteForms = document.querySelectorAll('.delete-form')
+// Modale
 const modal = document.getElementById('delete-modal');
+const modalText = document.querySelector('.modal-body');
+// Pulsante conferma nella modale
 const confirmButton = document.querySelector('.confirm-btn');
+// Pulsante chiusura nella modale
 const closeButton = document.getElementById('button-close');
 
 deleteForms.forEach(form => {
     form.addEventListener('submit', e => {
         e.preventDefault();
         modal.style = "display: block"
+        modalText.innerText += ` ${form.dataset.title}?`;
         confirmButton.addEventListener('click', () => {
             form.submit();
         })
