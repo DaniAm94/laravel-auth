40ad2fef10baff017eaed855d3df3812aa118b11
DaniAm94 <dani.amico94@gmail.com>
2024-03-20 18:47:27 +0100

    added a function to the model project to format its date

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,9 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
 class Project extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    protected $fillable = ['title', 'description', 'slug', 'is_completed', 'image'];
+
+    public function getFormattedDate($column, $format = 'd-m-Y')
+    {
+        return Carbon::create($this->$column)->format($format);
+    }
 }
