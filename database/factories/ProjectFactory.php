ba0fd63a1825eb4b20772da3120293158759e4e8
DaniAm94 <dani.amico94@gmail.com>
2024-03-22 23:10:58 +0100

    updated project factory to get local images

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 /**
@@ -18,11 +19,15 @@ class ProjectFactory extends Factory
     public function definition(): array
     {
         $title = fake()->text(20);
+        $slug = Str::slug($title);
+        Storage::makeDirectory('project_images');
+        $image = fake()->image(null, 250, 250, 'motorcycles');
+        $image_url = Storage::putFileAs('project_images', $image, "$slug.png");
         return [
             'title' => $title,
-            'slug' => Str::slug($title),
+            'slug' => $slug,
             'description' => fake()->paragraphs(15, true),
-            'image' => fake()->imageUrl(250, 250, true),
+            'image' => $image_url,
             'is_completed' => fake()->boolean()
         ];
     }
